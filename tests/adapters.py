ea765e20a310950d5ef94a865edc310f78f6da92
123 <123@gmail.com>
2025-10-15 00:34:37 +0800

    finish one process bpe algorithm

--- tests/adapters.py
+++ tests/adapters.py
@@ -10,6 +10,11 @@ from jaxtyping import Bool, Float, Int
 from torch import Tensor
 
 
+
+# invoke my implementations here
+from cs336_basics.bpe import train_bpe
+
+
 def run_linear(
     d_in: int,
     d_out: int,
@@ -589,4 +594,6 @@ def run_train_bpe(
                 representing that <token1> was merged with <token2>.
                 Merges are ordered by order of creation.
     """
-    raise NotImplementedError
+    return train_bpe(input_path,vocab_size,special_tokens, **kwargs)
+    
+    
--- tests/test_train_bpe.py
+++ tests/test_train_bpe.py
@@ -21,7 +21,10 @@ def test_train_bpe_speed():
         special_tokens=["<|endoftext|>"],
     )
     end_time = time.time()
+    print(f"BPE training took {end_time - start_time} seconds")
     assert end_time - start_time < 1.5
+
+
 
 
 def test_train_bpe():
